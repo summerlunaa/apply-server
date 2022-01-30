b1fa3ad4ed79a4993e981cd7c724a94dac6452cf
myway00 <junior0614@naver.com>
2022-01-30 15:13:43 +0900

    카카오 로그인,회원가입 / 일반 로그인, 회원가입 구현 완료

---
+++ user/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns= [
+
+    path('login/home/', login_home, name="login_home"),
+
+    path('signup/email/', signup_email, name="signup_email"), 
+    path('login/email/', email_login, name="email_login"), 
+
+
+    path('login/kakao/', kakao_login, name="kakao_login"),
+    path('kakao/login/callback/', kakao_login_callback, name="kakao_login_callback"), 
+    path('kakao/submit/', submit_kakao, name="submit_kakao"),
+
+    path('logout/', logout_view, name="logout"), 
+    path('logout/with/kakao', logout_with_kakao, name="logout_with_kakao"),
+
+    path('success/', success, name = "success"),
+]
