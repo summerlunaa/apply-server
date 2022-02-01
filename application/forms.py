d92971fc16c773e5e4cf46eee10caa160d364300
summerlunaa <summerlunaa9@gmail.com>
2022-02-01 14:57:59 +0900

    #5 Add: ApplicationForm 생성

--- application/forms.py
+++ application/forms.py
@@ -1,14 +1,9 @@
 from django import forms
-from .models import Answer, Portfolio
+from .models import Application
 
 
-class AnswerForm(forms.ModelForm):
+class ApplicationForm(forms.ModelForm):
     class Meta:
-        model = Answer
-        fields = ['content']
-
-
-class PortfolioForm(forms.ModelForm):
-    class Meta:
-        model = Portfolio
-        fields = ['file']
+        model = Application
+        fields = ['answer1', 'answer2', 'answer3', 'answer4', 'answer5', 'deadline',
+        'body', 'image']
