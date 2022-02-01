ad77e61e1227d9a72df2174af3cb1780e2d59492
summerlunaa <summerlunaa9@gmail.com>
2022-02-01 13:26:00 +0900

    #2 Fix: Application 모델 수정
    Portfolio를 Application 모델로 옮기고 created_at 삭제

--- application/models.py
+++ application/models.py
@@ -10,15 +10,7 @@ class Application(models.Model):
     answer3 = models.TextField(default='', blank=True)
     answer4 = models.TextField(default='', blank=True)
     answer5 = models.TextField(default='', blank=True)
-    created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
-
-    def __str__():
-        return user
-
-
-class Portfolio(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     file = models.FileField(null=True, blank=True)
 
     def __str__():
