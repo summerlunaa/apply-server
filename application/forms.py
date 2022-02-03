77f461b323438b40f43e98b619e3549f130fe722
summerlunaa <summerlunaa9@gmail.com>
2022-02-03 17:24:08 +0900

    #5 Fix: ApplicationForm 필드 수정

--- application/forms.py
+++ application/forms.py
@@ -5,5 +5,4 @@ from .models import Application
 class ApplicationForm(forms.ModelForm):
     class Meta:
         model = Application
-        fields = ['answer1', 'answer2', 'answer3', 'answer4', 'answer5', 'deadline',
-        'body', 'image']
+        fields = ['answer1', 'answer2', 'answer3', 'answer4', 'answer5', 'portfolio']
