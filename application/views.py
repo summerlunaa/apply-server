75284577bb748a81f22df4eeff1c2be0cb9af1dd
chans <papajuju123@naver.com>
2022-02-05 01:52:04 +0900

    #16 bug: 동영상 로딩

--- application/views.py
+++ application/views.py
@@ -6,27 +6,30 @@ from .forms import ApplicationForm
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "home.html")
 
 
 @login_required
 def write(request):
     application = Application.objects.filter(user=request.user).first()
-    
-    if request.method == 'POST':
-        if application: # 지원서 수정
-            form = ApplicationForm(request.POST, request.FILES, instance=application)
-        else:           # 지원서 첫 작성
+
+    if request.method == "POST":
+        if application:  # 지원서 수정
+            form = ApplicationForm(
+                request.POST, request.FILES, instance=application
+            )
+        else:  # 지원서 첫 작성
             form = ApplicationForm(request.POST, request.FILES)
         if form.is_valid():
             editApplication = form.save(commit=False)
             editApplication.user = request.user
             editApplication.updated_at = timezone.now()
             editApplication.save()
-            return redirect('application')
+            return redirect("application")
     else:
-        if application: # 지원서 수정
+        if application:  # 지원서 수정
             form = ApplicationForm(instance=application)
-        else:           # 지원서 첫 작성
+        else:  # 지원서 첫 작성
             form = ApplicationForm()
-    return render(request, 'application.html', {'form': form})
+    return render(request, "application.html", {"form": form})
+
