82db6e57602855b18382c0091c70518e37f218c4
summerlunaa <summerlunaa9@gmail.com>
2022-02-04 17:18:39 +0900

    #2 Add: Application 모델에 design_doc 파일 추가

--- application/forms.py
+++ application/forms.py
@@ -5,4 +5,4 @@ from .models import Application
 class ApplicationForm(forms.ModelForm):
     class Meta:
         model = Application
-        fields = ['answer1', 'answer2', 'answer3', 'answer4', 'answer5', 'portfolio']
+        fields = ['answer1', 'answer2', 'answer3', 'answer4', 'answer5', 'portfolio', 'design_dod']
