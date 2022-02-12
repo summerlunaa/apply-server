6bbe67ee69f616c5f0005eb4794d499e05fa14e7
gilgor2 <misby0327@gmail.com>
2022-02-12 19:30:39 +0900

    #36 add:done

--- application/static/js/home.js
+++ application/static/js/home.js
@@ -21,92 +21,134 @@ function scrollPlay() {
 }
 
 window.requestAnimationFrame(scrollPlay);
+var pageNum = 1;  
 
 //스크롤 애니메이션
 const onScroll = () => {
-  const page1Comp = document.querySelectorAll(".pagecomp1");
-  const page2Comp = document.querySelectorAll(".pagecomp2");
-  const page3Comp = document.querySelectorAll(".pagecomp3");
-  const page4Comp = document.querySelectorAll(".pagecomp4");
-  const page5Comp = document.querySelectorAll(".pagecomp5");
+  // const page1Comp = document.querySelectorAll(".pagecomp1");
+  // const page2Comp = document.querySelectorAll(".pagecomp2");
+  // const page3Comp = document.querySelectorAll(".pagecomp3");
+  // const page4Comp = document.querySelectorAll(".pagecomp4");
+  // const page5Comp = document.querySelectorAll(".pagecomp5");
+  const page1 = document.querySelector(".page1");
+  const page2 = document.querySelector(".page2");
+  const page3 = document.querySelector(".page3");
+  const page4 = document.querySelector(".page4");
+  const page5 = document.querySelector(".page5");
   const title = document.querySelector("#logo-title");
   const vid = document.getElementById("v0");
+  const down = document.querySelector("#down");
+
   var lastHeight = Math.floor(vid.duration) * playbackConst;
   var currentY = document.querySelector("html").scrollTop;
+  if (pageNum>=6){
+    down.style.transform="rotate(180deg)";
+  } else{
+    down.style.transform="rotate(0deg)";
 
+  }
   //스크롤 액션
   if (currentY >= 0 && currentY <= lastHeight / 5) {
-    console.log(1);
     title.style.transform = `translate(${
       40 - ((40 * currentY) / lastHeight) * 5
     }rem,${20 - ((20 * currentY) / lastHeight) * 5}rem) scale(${
       550 - 450 * ((currentY / lastHeight) * 5)
     }%) `;
-    for (comp of page1Comp) {
-      comp.classList.add("none");
-    }
-    for (comp of page2Comp) {
-      comp.classList.remove("none");
-    }
+    // page1.classList.add("none");
+
+    // for (comp of page1Comp) {
+    //   comp.classList.add("none");
+    // }
+    // for (comp of page2Comp) {
+    //   comp.classList.remove("none");
+    // }
+
+    page2.classList.remove("none");
+    pageNum = 1;
+    down.style.transform="rotate(0deg)"
   } else if (currentY > lastHeight / 5 && currentY <= (lastHeight / 5) * 2) {
-    console.log(2);
-    for (comp of page1Comp) {
-      comp.classList.remove("none");
-    }
-    for (comp of page2Comp) {
-      comp.classList.add("none");
-    }
-    for (comp of page3Comp) {
-      comp.classList.remove("none");
-    }
+    page2.classList.add("none");
+    page3.classList.remove("none");
+
+    // for (comp of page1Comp) {
+    //   comp.classList.remove("none");
+    // }
+    // for (comp of page2Comp) {
+    //   comp.classList.add("none");
+    // }
+    // for (comp of page3Comp) {
+    //   comp.classList.remove("none");
+    // }
+  pageNum =2;
   } else if (
     currentY > (lastHeight / 5) * 2 &&
     currentY <= (lastHeight / 5) * 3
   ) {
-    console.log(3);
-
-    for (comp of page2Comp) {
-      comp.classList.remove("none");
-    }
-    for (comp of page3Comp) {
-      comp.classList.add("none");
-    }
-    for (comp of page4Comp) {
-      comp.classList.remove("none");
-    }
+    page3.classList.add("none");
+    page2.classList.remove("none");
+    page4.classList.remove("none");
+    // for (comp of page2Comp) {
+    //   comp.classList.remove("none");
+    // }
+    // for (comp of page3Comp) {
+    //   comp.classList.add("none");
+    // }
+    // for (comp of page4Comp) {
+    //   comp.classList.remove("none");
+    // }
+  pageNum = 3;
   } else if (
     currentY > (lastHeight / 5) * 3 &&
     currentY <= (lastHeight / 5) * 4
   ) {
-    console.log(4);
-
-    for (comp of page3Comp) {
-      comp.classList.remove("none");
-    }
-    for (comp of page4Comp) {
-      comp.classList.add("none");
-    }
-    for (comp of page5Comp) {
-      comp.classList.remove("none");
-    }
+    page4.classList.add("none");
+    page3.classList.remove("none");
+    page5.classList.remove("none");
+    // for (comp of page3Comp) {
+    //   comp.classList.remove("none");
+    // }
+    // for (comp of page4Comp) {
+    //   comp.classList.add("none");
+    // }
+    // for (comp of page5Comp) {
+    //   comp.classList.remove("none");
+    // }
+  pageNum = 4;
   } else if (currentY > (lastHeight / 5) * 4 && currentY <= lastHeight ) {
-    console.log(5);
-
-    for (comp of page4Comp) {
-      comp.classList.remove("none");
-    }
-    for (comp of page5Comp) {
-      comp.classList.add("none");
-    }
+    page5.classList.add("none");
+    page4.classList.remove("none");
+    // page2.classList.remove("none");
+    // for (comp of page4Comp) {
+    //   comp.classList.remove("none");
+    // }
+    // for (comp of page5Comp) {
+    //   comp.classList.add("none");
+    // }
+  pageNum = 5;
   } else {
-    for (comp of page5Comp) {
-      comp.classList.remove("none");
-    }
+    page5.classList.remove("none");
+    
+    // for (comp of page5Comp) {
+    //   comp.classList.remove("none");
+    // }
+
+    pageNum=6;
+
   }
 };
 window.addEventListener("load", onScroll);
 window.addEventListener("scroll", onScroll);
+  //클릭시 스크롤
+const down=()=>{
+  const down = document.querySelector("#down");
 
+  window.scrollTo({
+    top: pageNum ===5?Math.floor(vid.duration) * playbackConst / 5 * (1 + pageNum++):pageNum===6?0:(Math.floor(vid.duration) * playbackConst / 5 * (pageNum))+1,
+    behavior: 'smooth',
+  });
+  
+  
+};
 //로딩화면
 const delLoad = () => {
   setTimeout(() => {
@@ -115,7 +157,6 @@ const delLoad = () => {
     window.scrollTo(0,0);
 
   }, 2000);
-  console.log("loaded");
 
 
 };
