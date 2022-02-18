e872ebca3896ce99cf53914026345ca32b4c29e8
eastyun <76711238+myway00@users.noreply.github.com>
2022-02-18 22:47:11 +0900

    \#91 ADD : 404 PAGE (#97)

--- applion_server/settings.py
+++ applion_server/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'applion_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
