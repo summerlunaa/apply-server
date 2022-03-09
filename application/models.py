573728a9c4e727c9898b5652077ed4238d0b8cbf
papajuju123@naver.com <papajuju123@naver.com>
2022-03-10 06:19:52 +0900

    #126 : application str position으로만

--- application/models.py
+++ application/models.py
@@ -15,4 +15,4 @@ class Application(models.Model):
     updated_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.user.name
+        return self.user.position
